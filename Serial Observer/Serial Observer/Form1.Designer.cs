7e40f83579cb1da8d59b647ee2b5f3fe1171d7f5
Jean-Daniel Massard <jean-daniel_massard@outlook.fr>
2018-04-01 16:47:00 +0200

    Ajout du dossier de projet

---
+++ Serial Observer/Serial Observer/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Serial_Observer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_portLogs = new System.Windows.Forms.ListBox();
+            this.comboBox_portName = new System.Windows.Forms.ComboBox();
+            this.comboBox_portSpeed = new System.Windows.Forms.ComboBox();
+            this.comboBox_portParity = new System.Windows.Forms.ComboBox();
+            this.comboBox_portData = new System.Windows.Forms.ComboBox();
+            this.comboBox_portStop = new System.Windows.Forms.ComboBox();
+            this.button_portConnect = new System.Windows.Forms.Button();
+            this.button_portDisconnect = new System.Windows.Forms.Button();
+            this.button_portSend = new System.Windows.Forms.Button();
+            this.textBox_textToSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox_portLogs
+            // 
+            this.listBox_portLogs.FormattingEnabled = true;
+            this.listBox_portLogs.Location = new System.Drawing.Point(12, 39);
+            this.listBox_portLogs.Name = "listBox_portLogs";
+            this.listBox_portLogs.Size = new System.Drawing.Size(629, 147);
+            this.listBox_portLogs.TabIndex = 0;
+            // 
+            // comboBox_portName
+            // 
+            this.comboBox_portName.FormattingEnabled = true;
+            this.comboBox_portName.Location = new System.Drawing.Point(12, 12);
+            this.comboBox_portName.Name = "comboBox_portName";
+            this.comboBox_portName.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_portName.TabIndex = 1;
+            // 
+            // comboBox_portSpeed
+            // 
+            this.comboBox_portSpeed.FormattingEnabled = true;
+            this.comboBox_portSpeed.Location = new System.Drawing.Point(139, 12);
+            this.comboBox_portSpeed.Name = "comboBox_portSpeed";
+            this.comboBox_portSpeed.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_portSpeed.TabIndex = 2;
+            // 
+            // comboBox_portParity
+            // 
+            this.comboBox_portParity.FormattingEnabled = true;
+            this.comboBox_portParity.Location = new System.Drawing.Point(393, 12);
+            this.comboBox_portParity.Name = "comboBox_portParity";
+            this.comboBox_portParity.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_portParity.TabIndex = 3;
+            // 
+            // comboBox_portData
+            // 
+            this.comboBox_portData.FormattingEnabled = true;
+            this.comboBox_portData.Location = new System.Drawing.Point(266, 12);
+            this.comboBox_portData.Name = "comboBox_portData";
+            this.comboBox_portData.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_portData.TabIndex = 4;
+            // 
+            // comboBox_portStop
+            // 
+            this.comboBox_portStop.FormattingEnabled = true;
+            this.comboBox_portStop.Location = new System.Drawing.Point(520, 12);
+            this.comboBox_portStop.Name = "comboBox_portStop";
+            this.comboBox_portStop.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_portStop.TabIndex = 5;
+            // 
+            // button_portConnect
+            // 
+            this.button_portConnect.Location = new System.Drawing.Point(12, 219);
+            this.button_portConnect.Name = "button_portConnect";
+            this.button_portConnect.Size = new System.Drawing.Size(248, 23);
+            this.button_portConnect.TabIndex = 6;
+            this.button_portConnect.Text = "Connecter";
+            this.button_portConnect.UseVisualStyleBackColor = true;
+            this.button_portConnect.Click += new System.EventHandler(this.button_portConnect_Click);
+            // 
+            // button_portDisconnect
+            // 
+            this.button_portDisconnect.Location = new System.Drawing.Point(266, 219);
+            this.button_portDisconnect.Name = "button_portDisconnect";
+            this.button_portDisconnect.Size = new System.Drawing.Size(248, 23);
+            this.button_portDisconnect.TabIndex = 7;
+            this.button_portDisconnect.Text = "Déconnecter";
+            this.button_portDisconnect.UseVisualStyleBackColor = true;
+            this.button_portDisconnect.Click += new System.EventHandler(this.button_portDisconnect_Click);
+            // 
+            // button_portSend
+            // 
+            this.button_portSend.Location = new System.Drawing.Point(520, 219);
+            this.button_portSend.Name = "button_portSend";
+            this.button_portSend.Size = new System.Drawing.Size(121, 23);
+            this.button_portSend.TabIndex = 8;
+            this.button_portSend.Text = "Envoyer";
+            this.button_portSend.UseVisualStyleBackColor = true;
+            this.button_portSend.Click += new System.EventHandler(this.button_portSend_Click);
+            // 
+            // textBox_textToSend
+            // 
+            this.textBox_textToSend.Location = new System.Drawing.Point(13, 193);
+            this.textBox_textToSend.Name = "textBox_textToSend";
+            this.textBox_textToSend.Size = new System.Drawing.Size(628, 20);
+            this.textBox_textToSend.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_textToSend);
+            this.Controls.Add(this.button_portSend);
+            this.Controls.Add(this.button_portDisconnect);
+            this.Controls.Add(this.button_portConnect);
+            this.Controls.Add(this.comboBox_portStop);
+            this.Controls.Add(this.comboBox_portData);
+            this.Controls.Add(this.comboBox_portParity);
+            this.Controls.Add(this.comboBox_portSpeed);
+            this.Controls.Add(this.comboBox_portName);
+            this.Controls.Add(this.listBox_portLogs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_portLogs;
+        private System.Windows.Forms.ComboBox comboBox_portName;
+        private System.Windows.Forms.ComboBox comboBox_portSpeed;
+        private System.Windows.Forms.ComboBox comboBox_portParity;
+        private System.Windows.Forms.ComboBox comboBox_portData;
+        private System.Windows.Forms.ComboBox comboBox_portStop;
+        private System.Windows.Forms.Button button_portConnect;
+        private System.Windows.Forms.Button button_portDisconnect;
+        private System.Windows.Forms.Button button_portSend;
+        private System.Windows.Forms.TextBox textBox_textToSend;
+    }
+}
+
---
+++ Serial Observer/Serial Observer/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serial_Observer
+{
+    public partial class Form1 : Form
+    {
+        // Variables
+        SerialPort serialPort;
+        AddListItem myDelegate;
+
+        // Methods
+        public delegate void AddListItem(String myString);
+
+        public Form1()
+        {
+            // Initialize
+            InitializeComponent();
+            InitializeComboBox();
+
+            // Form size
+            this.Size = new Size(670, 300);
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+            // Form options
+            this.Text = "Serial Observer";
+
+            // Buttons
+            modifyButtonsAccess(true, false, false);
+        }
+
+        /***************************** Events *****************************/
+        private void button_portConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Création d'un port avec les paramètres indiqués
+                serialPort = new SerialPort(comboBox_portName.Items[comboBox_portName.SelectedIndex].ToString(), parsePortSpeed(comboBox_portSpeed.Items[comboBox_portSpeed.SelectedIndex].ToString()), parsePortParity(comboBox_portParity.Items[comboBox_portParity.SelectedIndex].ToString()),parsePortData(comboBox_portData.Items[comboBox_portData.SelectedIndex].ToString()),parsePortStop(comboBox_portStop.Items[comboBox_portStop.SelectedIndex].ToString()));
+                
+                // Ouverture du port créé
+                serialPort.Open();
+
+                // Evénement à la réception de données
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                // Modification des boutons de contrôle
+                modifyButtonsAccess(false, true, true);
+
+                // Effacement du logger
+                listBox_portLogs.Items.Clear();
+
+                // Confirmation de réussite
+                MessageBox.Show("L'ouverture du port est un succès.", "Succès !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                // Une des listes déroulantes est vide
+                MessageBox.Show("Vous devez choisir une configuration !", "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_portDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort.Close();
+                modifyButtonsAccess(true, false, false);
+                MessageBox.Show("La fermeture du port est un succès.", "Succès !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_portSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort.Write(textBox_textToSend.Text);
+                textBox_textToSend.Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort sp = (SerialPort)sender;
+                string indata = sp.ReadLine();
+                myDelegate = new AddListItem(AddListItemMethod);
+                listBox_portLogs.Invoke(myDelegate, new Object[] { "[" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second + "] : " + indata });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /************************* Private Methods *************************/
+        private void InitializeComboBox()
+        {
+            // Port name
+            comboBox_portName.DropDownStyle = ComboBoxStyle.DropDownList;
+            string[] ports = SerialPort.GetPortNames();
+            foreach(string port in ports)
+            {
+                comboBox_portName.Items.Add(port);
+            }
+
+            // Port speed
+            comboBox_portSpeed.DropDownStyle = ComboBoxStyle.DropDownList;
+            int[] speeds = {1200,2400,4800,9600,14400,19200,28800,38400,57600,115200,230400};
+            foreach(int speed in speeds)
+            {
+                comboBox_portSpeed.Items.Add(speed);
+            }
+
+            // Port data
+            comboBox_portData.DropDownStyle = ComboBoxStyle.DropDownList;
+            int[] datas = {5,6,7,8};
+            foreach(int data in datas)
+            {
+                comboBox_portData.Items.Add(data);
+            }
+
+            // Port parity
+            comboBox_portParity.DropDownStyle = ComboBoxStyle.DropDownList;
+            String[] parities = {Parity.None.ToString(),Parity.Even.ToString(),Parity.Odd.ToString(),Parity.Mark.ToString(),Parity.Space.ToString()};
+            foreach(string parity in parities)
+            {
+                comboBox_portParity.Items.Add(parity);
+            }
+
+            // Port stop
+            comboBox_portStop.DropDownStyle = ComboBoxStyle.DropDownList;
+            String[] stops = {StopBits.None.ToString(),StopBits.One.ToString(),StopBits.OnePointFive.ToString(),StopBits.Two.ToString()};
+            foreach(string stop in stops)
+            {
+                comboBox_portStop.Items.Add(stop);
+            }
+        }
+
+        private int parsePortSpeed(string speed)
+        {
+            return (int.Parse(speed));
+        }
+
+        private Parity parsePortParity(string parity)
+        {
+            if(parity == "None")
+            {
+                return (Parity.None);
+            }
+            else if(parity == "Even")
+            {
+                return (Parity.Even);
+            }
+            else if(parity == "Odd")
+            {
+                return (Parity.Odd);
+            }
+            else if(parity == "Mark")
+            {
+                return (Parity.Mark);
+            }
+            else
+            {
+                return (Parity.Space);
+            }
+        }
+
+        private int parsePortData(string data)
+        {
+            return (int.Parse(data));
+        }
+
+        private StopBits parsePortStop(string stop)
+        {
+            if (stop == "None")
+            {
+                return (StopBits.None);
+            }
+            else if (stop == "One")
+            {
+                return (StopBits.One);
+            }
+            else if (stop == "OnePointFive")
+            {
+                return (StopBits.OnePointFive);
+            }
+            else
+            {
+                return (StopBits.Two);
+            }
+        }
+
+        private void modifyButtonsAccess(Boolean _buttonPortConnect, Boolean _buttonPortDisconnect, Boolean _buttonPortSend)
+        {
+            button_portConnect.Enabled = _buttonPortConnect;
+            button_portDisconnect.Enabled = _buttonPortDisconnect;
+            button_portSend.Enabled = _buttonPortSend;
+        }
+
+        private void AddListItemMethod(String myString)
+        {
+            listBox_portLogs.Items.Add(myString);
+        }
+    }
+}
